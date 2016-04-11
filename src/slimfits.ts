1a4b63b3d71b53e9eb58650a2e73703f8225dc30
xmichaelx <michal.drzal@gmail.com>
2016-04-11 08:28:55 +0200

    Exposed Keywordmanager

--- src/slimfits.ts
+++ src/slimfits.ts
@@ -2,3 +2,4 @@ export {FitsReader} from './FitsReader';
 export {BlobFile} from './datasource/BlobFile';
 export {MultipleRequestFile} from './datasource/MultipleRequestFile';
 export {SingleRequestFile} from './datasource/SingleRequestFile';
+export {KeywordsManager} from './utils/KeywordsManager';
