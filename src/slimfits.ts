e723ea67700d80a60630bc650764443ff3db7d45
Michał Drzał <michal.drzal@sybillatechnologies.com>
2017-08-02 23:43:06 +0200

    Removed slimfits

--- src/slimfits.ts
+++
@@ -1,7 +0,0 @@
-export {FitsReader} from './FitsReader';
-export {BlobFile} from './datasource/BlobFile';
-export {MultipleRequestFile} from './datasource/MultipleRequestFile';
-export {SingleRequestFile} from './datasource/SingleRequestFile';
-export {ArrayBufferFile} from './datasource/ArrayBufferFile';
-export {KeywordsManager} from './utils/KeywordsManager';
-export {SphericalProjectionConvertersBuilder, IPlaneProjectionDefinition} from './wcs/SphericalProjectionConverters';
